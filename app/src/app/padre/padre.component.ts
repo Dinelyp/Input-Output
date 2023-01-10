ac4f92b1e2d455e7b79a30b372a0e6571e9880c3
Dinelyp <dinely@rapihost.com>
2023-01-09 19:32:12 -0500

    subiendo tarea

---
+++ app/src/app/padre/padre.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-padre',
+  templateUrl: './padre.component.html',
+  styleUrls: ['./padre.component.css']
+})
+export class PadreComponent implements OnInit {
+  title : string = 'Registro de Personas';
+  model={
+    nombre:"",
+    dni:"",
+    estado_civil:""
+  }
+  constructor(){
+
+  } 
+
+  ngOnInit(): void {
+    
+  }
+  valores=""
+
+ enviar(values:any):void{
+  console.log('from.values:',values);
+  this.valores=values;
+  
+
+}
+}
